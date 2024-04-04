e9b784dcf4bc18978abf86009ecb3aba5100908d
Jacques KOZIK <jacques.kzk@gmail.com>
2024-04-04 17:17:52 +0200

    COMMIT: 04/04/2024

--- notebook.py
+++ notebook.py
@@ -7,19 +7,15 @@ import nbformat as nbf
 
 
 for i in range(1,101):
-    # Create a new Jupyter notebook
+    #création d'un nouveau notebook
     notebook = nbf.v4.new_notebook()
 
-    # Read the CSV file
+    # lecture du fichier csv
     df = pd.read_csv("simulation_results_"+str(i)+".csv", names=["nombre_infected", "nombre_sain", "nombre_exposed", "nombre_recovered"])
 
-    # For each row in the DataFrame
     for index, row in df.iterrows():
-        # Create a new cell in the notebook
         cell = nbf.v4.new_markdown_cell(source="## Simulation results")
         notebook.cells.append(cell)
-
-        # Write the row to the cell
         cell.source = str(row)
 
     with open("simulation_results_"+str(i)+".ipynb", "w") as f:
